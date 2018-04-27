86e593120240c96a271a77fb19c9ab3c8b7b75b5
Jorge <jorge.costa@developer.pt>
2018-04-27 13:11:00 +0100

    Implemented button in the block toolbar that allows to move selection to the parent.

--- editor/components/block-list/breadcrumb.js
+++ editor/components/block-list/breadcrumb.js
@@ -6,16 +6,15 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { compose, Component } from '@wordpress/element';
-import { IconButton, Tooltip, Toolbar } from '@wordpress/components';
-import { withDispatch, withSelect } from '@wordpress/data';
-import { __ } from '@wordpress/i18n';
+import { Component } from '@wordpress/element';
+import { Toolbar } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import NavigableToolbar from '../navigable-toolbar';
 import BlockTitle from '../block-title';
+import BlockSelectParent from '../block-select-parent';
 
 /**
  * Block breadcrumb component, displaying the label of the block. If the block
@@ -26,7 +25,7 @@ import BlockTitle from '../block-title';
  * @param {string}   props.rootUID         UID of block's root.
  * @param {Function} props.selectRootBlock Callback to select root block.
  */
-export class BlockBreadcrumb extends Component {
+export default class BlockBreadcrumb extends Component {
 	constructor() {
 		super( ...arguments );
 		this.state = {
@@ -36,15 +35,10 @@ export class BlockBreadcrumb extends Component {
 		this.onBlur = this.onBlur.bind( this );
 	}
 
-	onFocus( event ) {
+	onFocus( ) {
 		this.setState( {
 			isFocused: true,
 		} );
-
-		// This is used for improved interoperability
-		// with the block's `onFocus` handler which selects the block, thus conflicting
-		// with the intention to select the root block.
-		event.stopPropagation();
 	}
 
 	onBlur() {
@@ -54,48 +48,22 @@ export class BlockBreadcrumb extends Component {
 	}
 
 	render( ) {
-		const { uid, rootUID, selectRootBlock, isHidden } = this.props;
+		const { uid, isHidden } = this.props;
 		const { isFocused } = this.state;
-		const selectParentLabel = __( 'Select parent block' );
 
 		return (
 			<NavigableToolbar className={ classnames( 'editor-block-list__breadcrumb', {
 				'is-visible': ! isHidden || isFocused,
 			} ) }>
 				<Toolbar>
-					{ rootUID && (
-						<Tooltip text={ selectParentLabel }>
-							<IconButton
-								onClick={ selectRootBlock }
-								onFocus={ this.onFocus }
-								onBlur={ this.onBlur }
-								label={ selectParentLabel }
-								icon="arrow-left-alt"
-							/>
-						</Tooltip>
-					) }
+					<BlockSelectParent
+						uid={ uid }
+						onFocus={ this.onFocus }
+						onBlur={ this.onBlur }
+					/>
 					<BlockTitle uid={ uid } />
 				</Toolbar>
 			</NavigableToolbar>
 		);
 	}
 }
-
-export default compose( [
-	withSelect( ( select, ownProps ) => {
-		const { getBlockRootUID } = select( 'core/editor' );
-		const { uid } = ownProps;
-
-		return {
-			rootUID: getBlockRootUID( uid ),
-		};
-	} ),
-	withDispatch( ( dispatch, ownProps ) => {
-		const { rootUID } = ownProps;
-		const { selectBlock } = dispatch( 'core/editor' );
-
-		return {
-			selectRootBlock: () => selectBlock( rootUID ),
-		};
-	} ),
-] )( BlockBreadcrumb );
